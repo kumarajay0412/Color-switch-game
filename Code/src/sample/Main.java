4152e722ff1889b415d7153a792c83d1f6f76044
robin828 <61390860+robin828@users.noreply.github.com>
2020-11-15 13:07:25 +0530

    first

--- Code/src/sample/Main.java
+++ Code/src/sample/Main.java
@@ -2,14 +2,50 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
+
+import java.io.Serializable;
+import java.util.ArrayList;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        final Circle circ = new Circle(100, 60, 10);
+        final Group root = new Group(circ);
+        primaryStage.setScene(new Scene(root, 400, 575, Color.BEIGE));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("op1");
+        launch(args);
+        System.out.println("op2");
+        Best b = new Best();
+        b.startGame();
+
+
+    }
+}
+
+class Best extends Application {
+
+    void startGame() {
+
+    }
+
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
@@ -17,7 +53,27 @@ public class Main extends Application {
     }
 
 
-    public static void main(String[] args) {
-        launch(args);
+}
+
+class runGame implements Serializable {
+    Ball ball = new Ball();
+    public int totalStar;
+    public int currentStar;
+    ArrayList<OP> gameData = new ArrayList<>();
+
+    void start() {
+
     }
+}
+
+class Ball {
+
+    private float yHieght;
+
+
+
+}
+
+interface OP {
+
 }
