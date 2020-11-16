7fa1b422e8ecce4a89913414de45f6a475a75249
kumarajay0412 <66991625+kumarajay0412@users.noreply.github.com>
2020-11-17 00:09:11 +0530

    test

--- Code/src/sample/Main.java
+++ Code/src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Color switch");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
