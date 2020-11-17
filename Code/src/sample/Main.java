3aebbb5b1c66ad06fe5a2535b3f1bf4b64f66be6
robin828 <61390860+robin828@users.noreply.github.com>
2020-11-17 11:25:01 +0530

    testing git

--- Code/src/sample/Main.java
+++ Code/src/sample/Main.java
@@ -2,22 +2,34 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-<<<<<<< Updated upstream
-=======
+
         //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Color Switch");
-        final Circle circ = new Circle(100, 60, 10);
-        final Group root = new Group(circ);
+        //final Circle circ = new Circle(100, 60, 10);
+        Best b1;
+        b1 = new Best();
+        Group root = new Group(b1.mainMenu());
+
+
+
+
         primaryStage.setScene(new Scene(root, 400, 575, Color.BEIGE));
         primaryStage.show();
+
     }
 
 
@@ -32,25 +44,28 @@ public class Main extends Application {
     }
 }
 
-class Best extends Application {
+class Best{
 
     void startGame() {
 
     }
 
-
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
->>>>>>> Stashed changes
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Color switch");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    VBox mainMenu(){
+        Button button1 = new Button("Start Game");
+        Button button2 = new Button("Load Game");
+        VBox vBox = new VBox(button1, button2);
+        vBox.setSpacing(10);
+        vBox.setAlignment(Pos.CENTER_RIGHT);
+        return vBox;
     }
 
 
-    public static void main(String[] args) {
-        launch(args);
+
+}
+
+class Ball {
+    Circle position() {
+        return new Circle(100, 60, 10);
     }
+
 }
